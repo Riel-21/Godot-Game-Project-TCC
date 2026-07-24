0b9eca559bfbedfa5ea6d05db6538b445f76e046
therealclarkhouse-sudo <therealclarkhouse@gmail.com>
2026-07-23 22:48:45 -0700

    tutorial_addition

---
+++ Scenes/tutorial.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
+
+
+
+func _on_change_to_main_body_entered(_body: Node2D) -> void:
+	get_tree().call_deferred("change_scene_to_file", "res://Scenes/castle_scene.tscn")
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -14,7 +14,7 @@ extends CharacterBody2D
 
 # --- MOVEMENT CONSTANTS ---
 const SPEED = 300.0
-const JUMP_VELOCITY = -300.0
+const JUMP_VELOCITY = -400.0
 const JUMP_CUTOFF = 0.5 
 
 # Wall Jump Tuning
