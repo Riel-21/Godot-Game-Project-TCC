0a2943b59700015d4cd779657d9eed60a5b4b752
therealclarkhouse-sudo <therealclarkhouse@gmail.com>
2026-07-25 19:51:17 -0700

    added_boss_damage
    added damage to the homing missile and the melee attack and the dash

--- Scripts/scripts/bullet.gd
+++ Scripts/scripts/bullet.gd
@@ -21,5 +21,7 @@ func _physics_process(delta):
 	
 	position += velocity * delta
 
-func _on_body_entered(_body: Node2D) -> void:
+func _on_body_entered(body: Node2D) -> void:
+	if body.has_method("take_damage"):
+		body.take_damage()
 	queue_free()
--- Scripts/scripts/melee_attack.gd
+++ Scripts/scripts/melee_attack.gd
@@ -2,16 +2,37 @@ extends State
 
 @export var damage: int = 15  # Adjust this value in the Godot Inspector!
 
+@onready var hitbox = $AttackHitbox
+var can_transition
 
 func enter():
 	super.enter()
+	hitbox.visible = false
 	animation_player.play("melee_attack")
+	can_transition = false
+	# Queue a timer
+	await get_tree().create_timer(1).timeout
+	# show hitbox
+	hitbox.visible = true
+	await get_tree().create_timer(.1).timeout
 	
+	can_transition = true
 	
 
-
+# func connected to melee collider signal
+	# Do damage
 
 
 func transition():
-		if owner.direction.length() > 30:
-			get_parent().change_state("Follow")
+	if !can_transition:
+		return
+		
+	if owner.direction.length() > 30:
+		get_parent().change_state("Follow")
+
+
+
+
+func _on_attack_hitbox_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player") and body.has_method("take_damage"):
+		body.take_damage()
